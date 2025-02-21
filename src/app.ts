135f0cd82b96f7bbc0c39e7c24c94958315475d8
Andrew Larson <lars1856@Andrews-MacBook-Pro.local>
2025-02-21 16:09:13 -0600

    Move data access to the backend

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,40 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 
+const COMLINK_API_URL = process.env.REACT_APP_COMLINK_API_URL;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", async (_req: Request, res: Response) => {
+  console.log(COMLINK_API_URL);
+  try {
+    const guildData = await fetch(`${COMLINK_API_URL}/getEvents`);
+    if (!guildData.ok) {
+      throw new Error(
+        `Response status: ${guildData.status} ${guildData.statusText}`
+      );
+    }
+    const guildJson = await guildData.json();
+    console.log(guildJson);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  try {
+    const guildData = await fetch(
+      "https://swgoh.gg/api/guild-profile/AffwMH3fQlSKrr0HfHR2pA"
+    );
+    if (!guildData.ok) {
+      throw new Error(
+        `Response status: ${guildData.status} ${guildData.statusText}`
+      );
+    }
+    const guildJson = await guildData.json();
+    console.log(guildJson);
+  } catch (error) {
+    console.error("Error:", error);
+  }
   res.json({ message: "API is running with TypeScript!", time: 0 });
 });
 
